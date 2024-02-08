89bda87a7de4005a7ce1a04f6f6698e85ae559b9
danicostas-xyz <daniel.costas.rodriguez@bootcamp-upgrade.com>
2024-02-08 13:40:36 +0100

    Act. team with methods

--- 19_ClasesObjetosArrays2/src/entidad/Team.java
+++ 19_ClasesObjetosArrays2/src/entidad/Team.java
@@ -3,19 +3,18 @@ package entidad;
 import java.util.Arrays;
 
 public class Team {
-	
+
 	// 1. Atributos
-	
+
 	String name;
 	String[] players;
 
-	
 	// 2. Constructores
-	
+
 	public Team() {
 		super();
 	}
-	
+
 	public Team(String name, String[] players) {
 		super();
 		this.name = name;
@@ -23,22 +22,58 @@ public class Team {
 	}
 
 	// 3. Métodos
-	
-	// Los objetos tendrán la opción de mostrar todos sus datos por pantalla (mediante el método toString())
-	
+
+	// -> Los objetos tendrán la opción de mostrar todos sus datos por pantalla
+	// (mediante el método toString())
+
 	@Override
 	public String toString() {
 		return "Equipo [nombre=" + name + ", jugadores=" + Arrays.toString(players) + "]";
 	}
+
+	// -> Los objetos tendrán la opción de mostrar todos sus jugadores
+	
+	public void printPlayers() {
+		for (int i = 0; i < this.players.length; i++) {
+			System.out.println("Jugador " + i + ": " + this.players[i]);
+		}
+	}
+	
+	// -> Los objetos tendrán la opción de devolver si existe un jugador pasado por
+	// parámetro.
+	
+	public String returnPlayerIfExists(String player) {
+		for (int i = 0; i < this.players.length; i++) {
+			if (this.players[i].equalsIgnoreCase(player)) {
+				player = this.players[i];
+			}
+		}
+		return player;
+	}
+
+	// -> Los objetos tendrán la opción de devolver el numero de jugadores que tienen.
 	
 	
 	
 	
 	
+	// -> Los objetos tendrán la opción de devolver si el número de jugadores que
+	// tienen es apto para jugar. Un equipo es apto para jugar si tiene al menos 7
+	// jugadores.
 	
 	
 	
 	
 	
+	// -> Los objetos tendrán la opción de devolver si una lista de jugadores pasada
+	// por parámetro es exactamente igual a su lista de jugadores
 	
+	
+	
+	
+	
+	// -> Los objetos tendrán la opción de devolver si un equipo pasado por parámetro
+	// es exactamente igual al mismo. Un equipo es exactamente igual si tiene el
+	// mismo nombre y la misma lista de jugadores con los mismos nombres.
+
 }
