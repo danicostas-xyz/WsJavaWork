c62fed04b6ffc1b33247cf5471bbf2fa3698b255
danicostas-xyz <daniel.costas.rodriguez@bootcamp-upgrade.com>
2024-04-09 11:15:05 +0200

    Modificación de calculadora para que funcione correctamente hasta que se seleccione el fin del programa

--- 24-2_MatematicasConLibreria/src/Main.java
+++ 24-2_MatematicasConLibreria/src/Main.java
@@ -8,43 +8,51 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("CALCULADORA");
-		System.out.println("-----------");
+		System.out.println("  -----------  ");
+		System.out.println("| CALCULADORA |");
+		System.out.println("  -----------  ");
 
 		printMenu();
+		System.out.println("Fin del programa");
 
 	}
 
 	public static void printMenu() {
-		System.out.print("Introduce el primer número: ");
-		int n1 = Integer.parseInt(sc.nextLine());
-
-		System.out.print("Introduce el segundo número: ");
-		int n2 = Integer.parseInt(sc.nextLine());
-
 		int seleccion = 0;
+		int resultado = 0;
+
 		do {
 			System.out.println("");
 			System.out.println("Elige la operación a realizar: ");
+			System.out.println("");
 			System.out.println("1. Suma");
 			System.out.println("2. Resta");
 			System.out.println("3. Multiplicación");
 			System.out.println("4. División");
 			System.out.println("5. Salir del programa");
+			System.out.println("");
 
 			seleccion = Integer.parseInt(sc.nextLine());
-			int resultado = 0;
+			System.out.println("");
 
-			if (seleccion == 5) {
-				System.out.println("Fin del programa");
-			} else {
+			int n1 = 0;
+			int n2 = 0;
+
+			if (seleccion != 5) {
+				System.out.print("Introduce el primer número: ");
+				n1 = Integer.parseInt(sc.nextLine());
+
+				System.out.print("Introduce el segundo número: ");
+				n2 = Integer.parseInt(sc.nextLine());
+
 				resultado = calculo(n1, n2, seleccion);
+
+				System.out.println("");
+				System.out.println("Resultado = " + resultado);
 			}
 
-			System.out.println("Resultado = " + resultado);
-			
 		} while (seleccion != 5);
-		
+
 	}
 
 	public static int calculo(int n1, int n2, int seleccion) {
